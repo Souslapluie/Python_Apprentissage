b64b3f938c6337a66f5c602e791e205cd843a02d
Souslapluie <77190496+Souslapluie@users.noreply.github.com>
2021-03-23 15:16:41 +0100

    Update SecurePasskey.py

--- SecurePasskey.py
+++ SecurePasskey.py
@@ -1,76 +1,72 @@
 """
-
-
 				Ici, nous voulons améliorer l'intimité de nos clefs de passe.
-
-
 	Commandes claviers en public :
 		
     Maj + Shift = Annulation des deux = Tromper quiconque regardant le clavier en tapant
-
     Caractère spécial du style ' Ï '  = Tromper quiconque regardant le clavier en tapant
-
     Avoir son propre "langage"+random = Tromper quiconque regardant le clavier en tapant
-
     S'entraîner à taper à vitesse V   = Tromper quiconque regardant le clavier en tapant
-
     
     
-
 		Il faut une clef de passe la plus éloignée de notre vie et de notre personne 
 			pour diminuer les pistes de l'inginérie sociale.
-
-
 			Les ondes sinusoïdales constitutives.
-
-
 	    	 =  |_35 0nd35 51n|_|50Ïd4|es [onstituti\/e.e.s.
-
-
 			GOTTA GO FAST = |_35 0nd35 51n|_|50Ïd4|35 [onstituti\e.e.s.
-
-
 			1 - |_35 0nd35 51n|_|50Ïd4|es [onstituti\/e.e.s.
-
-
 			2 -
-
-
 			3 - 
-
-
 			4 -
-
-
 			5 -
 		
-
 """
 
 from datetime import datetime
-print("Début\n")
+import time
+from time import sleep
 
-while True:
-	SPass = input("\nEntrez votre clef de passe sur lequel vous souhaitez vous entraîner. Soyez attentif : ")
+print("\n             Bienvenue au camp d'entraînement. ")
+print("                  L'objectif étant de trouver la clef la plus impénétrable possible.")
+print("                     Et de s'entraîner à la taper le plus rapidement possible lorsque d'autres personnes sont à proximité.")
 
-	print("\nVous allez vous entraîner sur la clef de passe suivante : ", SPass)
 
-	Confirm = input ("\nConfirmez que c'est bel et bien avec cette clef de passe que vous allez vous entraîner (Y/N ou Q pour quitter) : ")
+#while True:
 
-	if Confirm.upper() == 'Y' :
-		timestamp1 = datetime.timestamp(datetime.now())
-		VPass = input("\nEntraînement de tapage de mot de passe ! Ecrivez-le, le plus rapidement possible : ")
-		timestamp2 = datetime.timestamp(datetime.now())
-		if VPass == SPass :
-			result = timestamp2 - timestamp1
-			print("\nBingo vous avez gagné : Temps d'exécution : ", result)
-			break
+SPass = input("\nEntrez votre clef de passe sur lequel vous souhaitez vous entraîner.\nSoyez attentif : ")
 
-	elif Confirm.upper() == 'Q' :
-		break
+print("\nVous allez vous entraîner sur la clef de passe suivante : ", SPass)
 
-	elif Confirm.upper() == 'N' :
-		return
+Confirm = input ("\nConfirmez que c'est bel et bien avec cette clef de passe que vous allez vous entraîner.\n(Y/N ou Q pour quitter) : ")
+
+if Confirm.upper() == 'Y' :
+    print("\nEntraînement de tapage de mot de passe ! Ecrivez-le, le plus rapidement possible.\n")
+    print("           Commencement au 'GO' dans 4 secondes :")
+    sleep(1)
+    print(" 3... ")
+    sleep(1)
+    print(" 2... ")
+    sleep(1)
+    print(" 1... ")
+    sleep(1)
+                
+    ChronoDeb = time.clock()
+    VPass = input("\n GO ! : ")
+    ChronoFin = time.clock()
+    result = ChronoFin - ChronoDeb
+            
+    elif VPass == SPass :
+                print("\nVous avez correctement recopié votre création. Temps d'exécution : ", result)
+                break
+
+    else VPass != SPass :
+                print("Votre création n'a pas été parfaitement recopiée. Temps d'exécution : ", result)
+                break
+
+if Confirm.upper() == 'Q' :
+            break
+
+else Confirm.upper() == 'N' :
+            exit()
 
 
 print("Fin")
